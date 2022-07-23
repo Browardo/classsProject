d9afce03cc32d3cf6a6d2c74bfbd74fd2518ed91
Shanon Sisson <shanonsisson@icloud.com>
2022-07-22 17:00:10 -0700

    startButtonActuallyStartsThaGAme!

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-
+let vary=1
+let button7=document.querySelector(".button7")
+button7.style["display"]="none"
+let introField7 = document.querySelector(".intro-field7")
+introField7.style["display"]="none"
 let gameTitle = "TITLE"
 console.log(gameTitle)
 document.querySelector(".Title").textContent = gameTitle
@@ -17,8 +21,7 @@ let chatButton= document.querySelector(".chat")
 let introField6=document.querySelector(".intro-field6")
 let chatMessage="  "
 
-let nextMessage=" "
-document.querySelector(".nextMessage")
+
 function sendMessage () {
     chatMessage=  "\n\nP1:   "+introField6.value+chatMessage 
     console.log(chatMessage)
@@ -45,8 +48,8 @@ let button5 = document.querySelector(".P5")
 let introField5 = document.querySelector(".intro-field5")
 let message5 = "  "
 
-let buttons = [button, button2, button3, button4, button5]
-let introFields = [introField, introField2, introField3, introField4, introField5]
+let buttons = [button, button2, button3, button4, button5, chatButton]
+let introFields = [introField, introField2, introField3, introField4, introField5, introField6]
 // 1
 button.onclick = function(){
     let intro = introField.value
@@ -91,9 +94,10 @@ startButton.onclick=function startGame() {
             introField.style["display"]="none"
         })
     buttons.forEach(function (button){
-            button.style["display"]="none"
+        //make atuff disapear    
+        button.style["display"]="none"
             })
-    
+        vary=2
             gameTitle= " "
             gameTitle2= " "
             gameTitle3= " "
@@ -101,6 +105,13 @@ startButton.onclick=function startGame() {
             document.querySelector(".Title1").textContent=gameTitle2
             document.querySelector(".Title2").textContent=gameTitle3
 
+            chatMessage=" "
+            
+            console.log ("chatMessageCleared")
+            document.querySelector(".chatMessage").textContent=chatMessage
+            //make stuff appear
+            introField7.style["display"]="block"
+            button7.style["display"]="block"
     }
 
     
@@ -109,9 +120,9 @@ chatButton.onclick = sendMessage
 
 document.onkeydown = function(event) {
     console.log(" pressed")
-    if (event.key == "Enter") {
+    if (vary===1) {if (event.key == "Enter") {
         console.log("enter pressed")
         sendMessage()
-    }
+    }}
 }
         
