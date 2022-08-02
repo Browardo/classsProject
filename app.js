bdd0a8966c68146260a728d1653ed9a1b5f3250d
Shanon Sisson <shanonsisson@icloud.com>
2022-08-02 16:03:07 -0700

    game assigns an impostor.

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-let yourMessage = ""
-let yourAvatar = 0
+function setVary3 () {
+    vary=3
+}
+
+function clearDaMessage() {
+    yourMessage=""
+document.querySelector(".your-message").textContent=yourMessage
+}
+
+let numOfPlayers=1
+let yourMessage = "You are not the Impostor.  Congrats. \n\n\n\n"
+let yourAvatar = numOfPlayers
+numOfPLayers=numOfPlayers=1
 let chatMessageFR=" "
 let vary=1
 let button7=document.querySelector(".button7")
@@ -29,17 +40,19 @@ function sendMessageBrody () {
     chatMessage= chatMessage +"P1:  " +"\n" + introField6.value  +  "\n\n"
     console.log(chatMessage)
     document.querySelector(".chatMessage").textContent=chatMessage
-    scroll(0,99999999999999999999999)
+    scroll(0,999999999999999999999999)
     introField6.value = ""
 }
 
 function sendMessageFR () {
+    if (vary === 3) {
     chatMessageFR= chatMessageFR + "P1:  " + "\n" + introField7.value + "\n\n"
     console.log(chatMessageFR)
     document.querySelector(".chatMessageFR").textContent=chatMessageFR
-    scroll(0,99999999999999999999999)
+    scroll(0,999999999999999999999999)
     introField7.value = ""
-}
+    waitAmoment()
+}}
 
 // 1 & 2
 let button = document.querySelector(".P1")
@@ -64,7 +77,7 @@ let buttons = [button, button2, button3, button4, button5, chatButton]
 let introFields = [introField, introField2, introField3, introField4, introField5, introField6]
 // 1
 button.onclick = function(){
-    yourAvatar = 1
+    //yourAvatar = 1
 
     message = "Player 1: " + introField.value
     console.log(message)
@@ -72,28 +85,28 @@ button.onclick = function(){
     }
 // 2
    button2.onclick = function (){
-    yourAvatar = 2
+    //yourAvatar = 2
     message2 = "Player 2: " + introField2.value
     console.log(message2)
     document.querySelector(".message2").textContent = message2
     }
 //3
 button3.onclick = function (){
-    yourAvatar = 3
+    //yourAvatar = 3
     message3 = "Player 3: " + introField3.value
     console.log(message3)
     document.querySelector(".message3").textContent = message3
     }
 //4
 button4.onclick = function (){
-    yourAvatar = 4
+    //yourAvatar = 4
     message4 = "Player 4: " + introField4.value
     console.log(message4)
     document.querySelector(".message4").textContent = message4
     }
 //5
 button5.onclick = function (){
-    yourAvatar = 5
+    //yourAvatar = 5
     console.log (yourAvatar)
     message5 = "Player 5: " + introField5.value
     console.log(message5)
@@ -101,13 +114,36 @@ button5.onclick = function (){
 }
 let startButton=document.querySelector(".Start")
 startButton.onclick=function startGame() {
+//pick impostor
+    
+
+      function random_rule(rules) {
+        return rules[Math.floor(Math.random()*rules.length)];
+      } 
+      const rules= ["Every message must contain 4 words or less", "Half of your messasges must be questions", "Every message must contain a prepositional phrase", "No punctuation"];
+      
+
+      function random_player(players) {
+        return players[Math.floor(Math.random()*players.length)];
+      } 
+      let players = [1,2,3,4,5]; 
+      console.log(random_player(players));
+      console.log (yourAvatar)
+      
+      if (yourAvatar === (random_player(players))) {yourMessage = "You are the Impostor!\nRule:\n" + [(random_rule(rules))]
+    }
+        
+              console.log (yourMessage)
+              document.querySelector(".your-message").textContent=yourMessage
+              setTimeout (clearDaMessage, 5000)
+//make stuff disapear    
     console.log ("start button pressed")
     startButton.style['display']="none"
     introFields.forEach(function (introField){
             introField.style["display"]="none"
         })
     buttons.forEach(function (button){
-        //make stuff disapear    
+        
         button.style["display"]="none"
             })
         vary=2
@@ -118,7 +154,7 @@ startButton.onclick=function startGame() {
             document.querySelector(".Title1").textContent=gameTitle2
             document.querySelector(".Title2").textContent=gameTitle3
 
-            chatMessage=" "
+            chatMessage=""
             
             console.log ("chatMessageCleared")
             document.querySelector(".chatMessage").textContent=chatMessage
@@ -127,36 +163,27 @@ startButton.onclick=function startGame() {
             button7.style["display"]="block"
             button7= document.querySelector(".button7")
             introField7=document.querySelector(".intro-field7")
-function random_player(players) {
-  return players[Math.floor(Math.random()*players.length)];
-} 
 
-  let players = [1,2,3,4,5]; 
-console.log(random_player(players));
-
-  let rule = "Every message has 4 words or less."
-            if (yourAvatar === random_player) {yourMessage = "You are the Impostor!\nRule:" + rule
-        console.log (yourMessage)}
-
-
-
+            setTimeout (setVary3, 6000)     
     }
 
-    
+ function waitAmoment()  {
+    vary=4
+    console.log ("the players converseth")
+    setTimeout (setVary3, 2000)
+ } 
     
 chatButton.onclick = sendMessageBrody
-button7.onclick = sendMessageFR
+button7.onclick = sendMessageFR  
 
 document.onkeydown = function(event) {
     if (vary===1) {if (event.key == "Enter") {
         console.log("enter pressed")
         sendMessageBrody()
     }} else {
-        if (vary===2) {if (event.key == "Enter") {
+         if (event.key == "Enter") {
             console.log("enter pressedFR")
             sendMessageFR()}
-        }}
+        }
         
 } 
-
-        
